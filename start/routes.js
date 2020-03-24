b487aa1d200b3db1ea8ba83ed4ffa2141f416a6e
Riva2000 <israelvillanueva6@hotmail.com>
2020-03-24 14:39:40 -0600

    ShopOnline Servidor

--- start/routes.js
+++ start/routes.js
@@ -22,4 +22,33 @@ Route.group(()=>{
     Route.post('login','UserController.login')
     Route.post('registro','UserController.registro')
     Route.get('mostrar/:id','UserController.mostrar')
+    Route.get('usuarios','UserController.index')
+    Route.get('productos','UserController.indexp')
+    Route.post('producto','UserController.insertProducto')
+    Route.delete('producto/:id','UserController.deleteProducto')
+
+    Route.get('ventas','UserController.indexv')
+    Route.post('venta','UserController.insertVenta')
+
+    Route.get('proveedores','UserController.indexsup')
+    Route.post('proveedor','UserController.insertProveedor')
+    Route.delete('proveedor/:id','UserController.deleteProveedor')
+
+    Route.get('clientes','UserController.indexc')
+    Route.post('cliente','UserController.insertCliente')
+    Route.delete('cliente/:id','UserController.deleteCliente')
+
+
+    Route.get('departamentos','UserController.indexd')
+    Route.post('departamento','UserController.insertDepartamento')
+    Route.delete('departamento/:id','UserController.deleteDepartamento')
+
+    Route.get('paqueterias','UserController.indexpaq')
+    Route.post('paqueteria','UserController.insertPaqueteria')
+    Route.delete('paqueteria/:id','UserController.deletePaqueteria')
+
+    Route.get('consulta','UserController.indexdv')
+    Route.post('detalle_venta','UserController.insertDetalle_Venta')
+    Route.delete('detalle_venta/:id','UserController.deleteDetalle_venta')
+
 }).prefix('usuarios')
